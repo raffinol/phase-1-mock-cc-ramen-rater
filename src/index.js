23a4ca8ecc5f5eac5c64dd824ef74487cc46fe1e
Rafael Finol <rafaelfinol88@gmail.com>
2023-04-30 09:42:13 -0400

    display images inside #ramen-menu div

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 // write your code here
+function showRamenImages(ramen) {
+  let img = document.createElement("img");
+  img.src = ramen.image;
+  document.getElementById("ramen-menu").appendChild(img);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("http://localhost:3000/ramens")
+    .then((resp) => resp.json())
+    .then((ramens) => ramens.forEach((ramen) => showRamenImages(ramen)));
+});
