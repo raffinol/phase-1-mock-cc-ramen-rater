81d830bb2105fa3897b3df882ea4a06b147f2b36
Rafael Finol <rafaelfinol88@gmail.com>
2023-04-30 17:53:01 -0400

    Show ramen detail after click

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
-// write your code here
 function showRamenImages(ramen) {
   let img = document.createElement("img");
   img.src = ramen.image;
   document.getElementById("ramen-menu").appendChild(img);
+  img.addEventListener("click", () => {
+    showRamenInfo(ramen);
+  });
+}
+
+function showRamenInfo(ramen) {
+  detailImage = document.querySelector("img.detail-image");
+  detailName = document.querySelector("h2.name");
+  detailRestaurant = document.querySelector("h3.restaurant");
+  detailRating = document.querySelector("span#rating-display");
+  detailComment = document.querySelector("p#comment-display");
+
+  detailImage.src = ramen.image;
+  detailName.textContent = ramen.name;
+  detailRestaurant.textContent = ramen.restaurant;
+  detailRating.textContent = ramen.rating;
+  detailComment.textContent = ramen.comment;
 }
 
 document.addEventListener("DOMContentLoaded", () => {
